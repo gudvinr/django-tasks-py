ca500e230b6b61a28a73afde91c9edef4c832607
gudvinr <gudvinr@gmail.com>
2017-05-05 04:42:25 +0300

    [*] fix enum for 3.5 compat

--- amsite/model/state.py
+++ amsite/model/state.py
@@ -1,16 +1,10 @@
-from enum import Enum, auto
+from enum import Enum
 
 
-class AutoName(Enum):
-
-    def _generate_next_value_(name, start, count, last_values):  # noqa
-        return name
-
-
-class State(AutoName):
+class State(Enum):
     '''
     Enum that defines possible states of Task
     '''
 
-    in_progress = auto()
-    ready = auto()
+    in_progress = 'in_progress'
+    ready = 'ready'
