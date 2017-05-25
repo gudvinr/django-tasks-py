5a38688f59a728bfcd7656ceeb6e4e20b67ed9e7
gudvinr <gudvinr@gmail.com>
2017-05-25 16:59:12 +0300

    set debug to False

--- amsite/settings.py
+++ amsite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!2k(iy)rrmnerty77*@^4&pcv5r)qu1ubbw68!zp*0%*1o=6)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
